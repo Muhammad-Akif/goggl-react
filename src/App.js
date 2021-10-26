21605c757d58115a7a5373fb6af662d27ce236c7
muhammad-akif <muhammadakif2917@gmail.com>
2021-10-26 16:10:39 +0500

    added Navbar Routes Footer comp and some code

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React, { useState } from 'react'
+import Navbar from './components/Navbar'
+import Routes from './components/Routes'
+import Footer from './components/Footer'
 
 function App() {
     const [darkTheme, setDarkTheme] = useState(false);
 
     return (
         <div className={darkTheme ? "dark" : ""}>
-            <div className="bg-gray-400">
-                Hello World
+            <div className="bg-gray-100 dark:bg-gray-900 dark:text-gray-200 min-h-screen">
+                <Navbar />
+                <Routes />
+                <Footer />
             </div>
         </div>
     )
