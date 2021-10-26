2f8b4580c1abaad28d7f8f9a078d1bfd74e3140f
muhammad-akif <muhammadakif2917@gmail.com>
2021-10-26 22:05:25 +0500

    created Loading Component & use context in results file

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -1,6 +1,16 @@
-import React from 'react'
+import React, { useEffect } from 'react';
+import { useLoaction } from 'react-router-dom';
+import ReactPlayer from 'react-player';
+import Loading from './Loading'
+
+import { useResultContext } from '../contexts/ResultContextProvider'
 
 const Results = () => {
+    const { results, isLoading, getResults, searchTerm } = useResultContext()
+    const location = useLoaction();
+
+    if(isLoading) return <Loading />;
+
     return (
         <div>
             React Results
