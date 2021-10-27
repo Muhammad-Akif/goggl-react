0ae717ee79cb8bc7a8ebf2d69712a59ac02d5cc0
muhammad-akif <muhammadakif2917@gmail.com>
2021-10-27 09:36:58 +0500

    added /images fetching functionality

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -1,21 +1,66 @@
 import React, { useEffect } from 'react';
-import { useLoaction } from 'react-router-dom';
-import ReactPlayer from 'react-player';
+import { useLocation } from 'react-router-dom';
+// import ReactPlayer from 'react-player';
 import Loading from './Loading'
 
 import { useResultContext } from '../contexts/ResultContextProvider'
 
 const Results = () => {
     const { results, isLoading, getResults, searchTerm } = useResultContext()
-    const location = useLoaction();
+    const location = useLocation();
 
-    if(isLoading) return <Loading />;
+    useEffect( () => {
+        if (searchTerm) {
+            if (location.pathname === '/videos') {
+                getResults(`/search/q=${searchTerm} videos`)
+            }
+            else {
+                getResults(`${location.pathname}/q=${searchTerm}&num=40`)
+            }
+            
+        }
+    },[searchTerm, location.pathname])
 
-    return (
-        <div>
-            React Results
-        </div>
-    )
+    if (isLoading) return <Loading />;
+
+    switch (location.pathname) {
+        case '/search':
+            return (
+                <div className="flex flex-wrap justify-between space-y-6 sm:px-56">
+                    { results?.results?.map(({ link, title}, index) => (
+                        <div key={index} className="md:w-2/5 w-full">
+                            <a href={link} target="_blank" rel="noreferrer">
+                                <p className="text-sm">
+                                    {link.length > 30 ? link.substring(0, 30) : link}
+                                </p>
+                                <p className="text-lg hover:underline dark:text-blue-300 text-blue-700">
+                                     { title }
+                                </p>
+                            </a>
+                        </div>
+                    ))}
+                </div>
+            );
+        case '/images':
+            return (
+                <div className="flex flex-wrap justify-center items-center">
+                    { results?.image_results?.map(({ image, link:{ href, title}}) => (
+                        <a className="sm:p-3 p-5" href={href} key={title} target="_blank" rel="noreferrer">
+                            <img src={image.src} alt={title} loader="lazy" />
+                            <p className="w-36 break-words text-sm mt-2">
+                                {title}
+                            </p>
+                        </a>
+                    ))}
+                </div>
+            );
+        case '/news':
+            return 'news';
+        case '/videos':
+            return 'videos';
+        default:
+            return 'Error';
+    }
 }
 
 export default Results
